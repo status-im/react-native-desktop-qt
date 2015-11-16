c69f95a8d556217b2ef755b97182fc508d329d88
Justin McPherson <justin.mcpherson@canonical.com>
2015-11-16 17:41:43 +1000

    WIP

--- ReactQML/reactvaluecoercion.h
+++ ReactQML/reactvaluecoercion.h
@@ -1,12 +1,7 @@
 #ifndef REACTVALUECOERCION_H
 #define REACTVALUECOERCION_H
 
-#include <functional>
-
 #include <QVariant>
-
-
-typedef std::function<QVariant (const QVariant&)> coerce_function;
 
 QVariant reactCoerceValue(const QVariant& data, int parameterType);
 
