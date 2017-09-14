64b50498a9d20a5a3c2b8834ba38ae058b67decc
Max Risuhin <risuhin.max@gmail.com>
2017-09-14 20:21:15 +0300

    refs #61 - Source code formatting check on ci build

--- ReactQt/application/src/main.cpp
+++ ReactQt/application/src/main.cpp
@@ -8,166 +8,165 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-#include <QUrl>
-#include <QGuiApplication>
 #include <QCommandLineParser>
+#include <QGuiApplication>
 #include <QQuickView>
+#include <QUrl>
 
 #include "reactattachedproperties.h"
 #include "reactflexlayout.h"
-#include "reacttextproperties.h"
-#include "reactrawtextproperties.h"
 #include "reactitem.h"
+#include "reactrawtextproperties.h"
+#include "reacttextproperties.h"
 #include "reactview.h"
-
 
 // TODO: some way to change while running
 class ReactNativeProperties : public QObject {
-  Q_OBJECT
-  Q_PROPERTY(bool liveReload READ liveReload WRITE setLiveReload NOTIFY liveReloadChanged)
-  Q_PROPERTY(QUrl codeLocation READ codeLocation WRITE setCodeLocation NOTIFY codeLocationChanged)
-  Q_PROPERTY(QString pluginsPath READ pluginsPath WRITE setPluginsPath NOTIFY pluginsPathChanged)
-  Q_PROPERTY(QString executor READ executor WRITE setExecutor NOTIFY executorChanged)
+    Q_OBJECT
+    Q_PROPERTY(bool liveReload READ liveReload WRITE setLiveReload NOTIFY liveReloadChanged)
+    Q_PROPERTY(QUrl codeLocation READ codeLocation WRITE setCodeLocation NOTIFY codeLocationChanged)
+    Q_PROPERTY(QString pluginsPath READ pluginsPath WRITE setPluginsPath NOTIFY pluginsPathChanged)
+    Q_PROPERTY(QString executor READ executor WRITE setExecutor NOTIFY executorChanged)
 public:
-  ReactNativeProperties(QObject* parent = 0): QObject(parent) {
-    m_codeLocation = m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort);
-  }
-  bool liveReload() const {
-    return m_liveReload;
-  }
-  void setLiveReload(bool liveReload) {
-    if (m_liveReload == liveReload)
-      return;
-    m_liveReload = liveReload;
-    Q_EMIT liveReloadChanged();
-  }
-  QUrl codeLocation() const {
-    return m_codeLocation;
-  }
-  void setCodeLocation(const QUrl& codeLocation) {
-    if (m_codeLocation == codeLocation)
-      return;
-    m_codeLocation = codeLocation;
-    Q_EMIT codeLocationChanged();
-  }
-  QString pluginsPath() const {
-    return m_pluginsPath;
-  }
-  void setPluginsPath(const QString& pluginsPath) {
-    if (m_pluginsPath == pluginsPath)
-      return;
-    m_pluginsPath = pluginsPath;
-    Q_EMIT pluginsPathChanged();
-  }
-  QString executor() const {
-    return m_executor;
-  }
-  void setExecutor(const QString& executor) {
-    if (m_executor == executor)
-      return;
-    m_executor = executor;
-    Q_EMIT executorChanged();
-  }
-  QString packagerHost() const {
-    return m_packagerHost;
-  }
-  void setPackagerHost(const QString& packagerHost) {
-    if (m_packagerHost == packagerHost)
-      return;
-    m_packagerHost = packagerHost;
-    setCodeLocation(m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort));
-  }
-  QString packagerPort() const {
-    return m_packagerPort;
-  }
-  void setPackagerPort(const QString& packagerPort) {
-    if (m_packagerPort == packagerPort)
-      return;
-    m_packagerPort = packagerPort;
-    setCodeLocation(m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort));
-  }
-  void setLocalSource(const QString& source) {
-    if (m_localSource == source)
-      return;
-
-    // overrides packager*
-    if (source.startsWith("file:")) {
-      setCodeLocation(source);
-    } else {
-      QFileInfo fi(source);
-      if (!fi.exists()) {
-        qWarning() << "Attempt to set non-existent local source file";
-        return;
-      }
-      setCodeLocation(QUrl::fromLocalFile(fi.absoluteFilePath()));
-      setLiveReload(false);
+    ReactNativeProperties(QObject* parent = 0) : QObject(parent) {
+        m_codeLocation = m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort);
     }
-  }
+    bool liveReload() const {
+        return m_liveReload;
+    }
+    void setLiveReload(bool liveReload) {
+        if (m_liveReload == liveReload)
+            return;
+        m_liveReload = liveReload;
+        Q_EMIT liveReloadChanged();
+    }
+    QUrl codeLocation() const {
+        return m_codeLocation;
+    }
+    void setCodeLocation(const QUrl& codeLocation) {
+        if (m_codeLocation == codeLocation)
+            return;
+        m_codeLocation = codeLocation;
+        Q_EMIT codeLocationChanged();
+    }
+    QString pluginsPath() const {
+        return m_pluginsPath;
+    }
+    void setPluginsPath(const QString& pluginsPath) {
+        if (m_pluginsPath == pluginsPath)
+            return;
+        m_pluginsPath = pluginsPath;
+        Q_EMIT pluginsPathChanged();
+    }
+    QString executor() const {
+        return m_executor;
+    }
+    void setExecutor(const QString& executor) {
+        if (m_executor == executor)
+            return;
+        m_executor = executor;
+        Q_EMIT executorChanged();
+    }
+    QString packagerHost() const {
+        return m_packagerHost;
+    }
+    void setPackagerHost(const QString& packagerHost) {
+        if (m_packagerHost == packagerHost)
+            return;
+        m_packagerHost = packagerHost;
+        setCodeLocation(m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort));
+    }
+    QString packagerPort() const {
+        return m_packagerPort;
+    }
+    void setPackagerPort(const QString& packagerPort) {
+        if (m_packagerPort == packagerPort)
+            return;
+        m_packagerPort = packagerPort;
+        setCodeLocation(m_packagerTemplate.arg(m_packagerHost).arg(m_packagerPort));
+    }
+    void setLocalSource(const QString& source) {
+        if (m_localSource == source)
+            return;
+
+        // overrides packager*
+        if (source.startsWith("file:")) {
+            setCodeLocation(source);
+        } else {
+            QFileInfo fi(source);
+            if (!fi.exists()) {
+                qWarning() << "Attempt to set non-existent local source file";
+                return;
+            }
+            setCodeLocation(QUrl::fromLocalFile(fi.absoluteFilePath()));
+            setLiveReload(false);
+        }
+    }
 Q_SIGNALS:
-  void liveReloadChanged();
-  void codeLocationChanged();
-  void pluginsPathChanged();
-  void executorChanged();
+    void liveReloadChanged();
+    void codeLocationChanged();
+    void pluginsPathChanged();
+    void executorChanged();
+
 private:
-  bool m_liveReload = false;
-  QString m_packagerHost = "localhost";
-  QString m_packagerPort = "8081";
-  QString m_localSource;
-  QString m_packagerTemplate = "http://%1:%2/index.ubuntu.bundle?platform=ubuntu&dev=true";
-  QUrl m_codeLocation;
-  QString m_pluginsPath;
-  QString m_executor = "ReactPipeExecutor";
+    bool m_liveReload = false;
+    QString m_packagerHost = "localhost";
+    QString m_packagerPort = "8081";
+    QString m_localSource;
+    QString m_packagerTemplate = "http://%1:%2/index.ubuntu.bundle?platform=ubuntu&dev=true";
+    QUrl m_codeLocation;
+    QString m_pluginsPath;
+    QString m_executor = "ReactPipeExecutor";
 };
 
-void registerTypes()
-{
-  qmlRegisterUncreatableType<ReactAttachedProperties>("React", 0, 1, "React", "React is not meant to be created directly");
-  qmlRegisterUncreatableType<ReactFlexLayout>("React", 0, 1, "Flex", "Flex is not meant to be created directly");
-  qmlRegisterUncreatableType<ReactTextProperties>("React", 0, 1, "Text", "Text is not meant to be created directly");
-  qmlRegisterUncreatableType<ReactRawTextProperties>("React", 0, 1, "RawText", "Text is not meant to be created directly");
-  qmlRegisterType<ReactItem>("React", 0, 1, "Item");
-  qmlRegisterType<ReactView>("React", 0, 1, "RootView");
+void registerTypes() {
+    qmlRegisterUncreatableType<ReactAttachedProperties>(
+        "React", 0, 1, "React", "React is not meant to be created directly");
+    qmlRegisterUncreatableType<ReactFlexLayout>("React", 0, 1, "Flex", "Flex is not meant to be created directly");
+    qmlRegisterUncreatableType<ReactTextProperties>("React", 0, 1, "Text", "Text is not meant to be created directly");
+    qmlRegisterUncreatableType<ReactRawTextProperties>(
+        "React", 0, 1, "RawText", "Text is not meant to be created directly");
+    qmlRegisterType<ReactItem>("React", 0, 1, "Item");
+    qmlRegisterType<ReactView>("React", 0, 1, "RootView");
 }
 
-int main(int argc, char** argv)
-{
-  QGuiApplication app(argc, argv);
-  QQuickView view;
-  ReactNativeProperties* rnp = new ReactNativeProperties(&view);
+int main(int argc, char** argv) {
+    QGuiApplication app(argc, argv);
+    QQuickView view;
+    ReactNativeProperties* rnp = new ReactNativeProperties(&view);
 
-  registerTypes();
+    registerTypes();
 
-  QCommandLineParser p;
-  p.setApplicationDescription("React Native host application");
-  p.addHelpOption();
-  p.addOptions({
-    {{"R", "live-reload"}, "Enable live reload."},
-    {{"H", "host"}, "Set packager host address.", rnp->packagerHost()},
-    {{"P", "port"}, "Set packager port number.", rnp->packagerPort()},
-    {{"L", "local"}, "Set path to the local packaged source", "not set"},
-    {{"M", "plugins-path"}, "Set path to node modules", "./plugins"},
-    {{"E", "executor"}, "Set Javascript executor", rnp->executor()},
-  });
-  p.process(app);
-  rnp->setLiveReload(p.isSet("live-reload"));
-  if (p.isSet("host"))
-    rnp->setPackagerHost(p.value("host"));
-  if (p.isSet("port"))
-    rnp->setPackagerPort(p.value("port"));
-  if (p.isSet("local"))
-    rnp->setLocalSource(p.value("local"));
-  if (p.isSet("plugins-path"))
-    rnp->setPluginsPath(p.value("plugins-path"));
-  if (p.isSet("executor"))
-    rnp->setExecutor(p.value("executor"));
+    QCommandLineParser p;
+    p.setApplicationDescription("React Native host application");
+    p.addHelpOption();
+    p.addOptions({
+        {{"R", "live-reload"}, "Enable live reload."},
+        {{"H", "host"}, "Set packager host address.", rnp->packagerHost()},
+        {{"P", "port"}, "Set packager port number.", rnp->packagerPort()},
+        {{"L", "local"}, "Set path to the local packaged source", "not set"},
+        {{"M", "plugins-path"}, "Set path to node modules", "./plugins"},
+        {{"E", "executor"}, "Set Javascript executor", rnp->executor()},
+    });
+    p.process(app);
+    rnp->setLiveReload(p.isSet("live-reload"));
+    if (p.isSet("host"))
+        rnp->setPackagerHost(p.value("host"));
+    if (p.isSet("port"))
+        rnp->setPackagerPort(p.value("port"));
+    if (p.isSet("local"))
+        rnp->setLocalSource(p.value("local"));
+    if (p.isSet("plugins-path"))
+        rnp->setPluginsPath(p.value("plugins-path"));
+    if (p.isSet("executor"))
+        rnp->setExecutor(p.value("executor"));
 
-  view.rootContext()->setContextProperty("ReactNativeProperties", rnp);
-  view.setSource(QUrl("qrc:///main.qml"));
-  view.setResizeMode(QQuickView::SizeRootObjectToView);
-  view.show();
+    view.rootContext()->setContextProperty("ReactNativeProperties", rnp);
+    view.setSource(QUrl("qrc:///main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.show();
 
-  return app.exec();
+    return app.exec();
 }
 
 #include "main.moc"
-
