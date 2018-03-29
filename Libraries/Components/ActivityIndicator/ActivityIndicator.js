fa5fa46ff235447de78dfbcc1562f62c6e08a20f
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-03-29 19:20:06 +0300

    Yoga build fixed

--- Libraries/Components/ActivityIndicator/ActivityIndicator.js
+++ Libraries/Components/ActivityIndicator/ActivityIndicator.js
@@ -48,20 +48,20 @@ const ActivityIndicator = createReactClass({
     ...ViewPropTypes,
     /**
      * Whether to show the indicator (true, the default) or hide it (false).
-     * 
+     *
      * See http://facebook.github.io/react-native/docs/activityindicator.html#animating
      */
     animating: PropTypes.bool,
     /**
      * The foreground color of the spinner (default is gray).
-     * 
+     *
      * See http://facebook.github.io/react-native/docs/activityindicator.html#color
      */
     color: ColorPropType,
     /**
      * Size of the indicator (default is 'small').
      * Passing a number to the size prop is only supported on Android.
-     * 
+     *
      * See http://facebook.github.io/react-native/docs/activityindicator.html#size
      */
     size: PropTypes.oneOfType([
@@ -72,7 +72,7 @@ const ActivityIndicator = createReactClass({
      * Whether the indicator should hide when not animating (true by default).
      *
      * @platform ios
-     * 
+     *
      * See http://facebook.github.io/react-native/docs/activityindicator.html#hideswhenstopped
      */
     hidesWhenStopped: PropTypes.bool,
@@ -145,7 +145,6 @@ if (Platform.OS === 'ios') {
   );
 } else if (Platform.OS === 'desktop') {
   var RCTActivityIndicator = requireNativeComponent('RCTActivityIndicatorView', ActivityIndicator);
-}
 }
 
 module.exports = ActivityIndicator;
