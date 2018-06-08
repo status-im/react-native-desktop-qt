5051a215db7a947e1d264acb13d32794031bf8fd
Madhav Varshney <madhavarshney@gmail.com>
2018-06-08 10:52:00 -0700

    More cleanup and conversion

--- Libraries/react-native-desktop/React.js
+++ Libraries/react-native-desktop/React.js
@@ -1,15 +1,13 @@
 /**
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) 2016-present, Facebook, Inc.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  *
- * @providesModule CheckBox
- * @flow
- * @format
  */
+
 'use strict';
 
-module.exports = require('UnimplementedView');
+module.exports = require('react');
