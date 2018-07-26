9c801eb26553718e28645710fc0e8e4c4b2fd023
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-07-26 13:38:04 +0300

    Added empty mock implementation for BackHandler (copied from ios)

--- Libraries/Utilities/BackHandler.desktop.js
+++ Libraries/Utilities/BackHandler.desktop.js
@@ -1,14 +1,35 @@
 /**
- * Copyright (c) 2017-present, Status Research and Development GmbH.
- * All rights reserved.
+ * Copyright (c) 2015-present, Facebook, Inc.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * On Apple TV, this implements back navigation using the TV remote's menu button.
+ * On iOS, this just implements a stub.
  *
  * @providesModule BackHandler
- * @flow
  */
+
 'use strict';
 
-module.exports = require('UnimplementedView');
+const Platform = require('Platform');
+
+type BackPressEventName = $Enum<{
+  backPress: string,
+}>;
+
+function emptyFunction() {}
+
+let BackHandler;
+
+BackHandler = {
+  exitApp: emptyFunction,
+  addEventListener() {
+    return {
+      remove: emptyFunction,
+    };
+  },
+  removeEventListener: emptyFunction,
+};
+
+module.exports = BackHandler;
