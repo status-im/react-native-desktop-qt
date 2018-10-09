da599d198c34fa47aa9fc7afce09b501b7ab593f
Max Risuhin <risuhin.max@gmail.com>
2018-10-10 00:44:21 +0300

    Don't remove scroll view model item on get request
    Signed-off-by: Max Risuhin <risuhin.max@gmail.com>

--- ReactQt/runtime/src/componentmanagers/scrollviewmodel.cpp
+++ ReactQt/runtime/src/componentmanagers/scrollviewmodel.cpp
@@ -5,7 +5,7 @@ ScrollViewModel::ScrollViewModel(QQmlEngine* engine, QObject* parent) : QAbstrac
     m_engine = engine;
 }
 
-int ScrollViewModel::rowCount(const QModelIndex& parent = QModelIndex()) const {
+int ScrollViewModel::rowCount(const QModelIndex& parent) const {
     return count();
 }
 
@@ -13,7 +13,7 @@ int ScrollViewModel::count() const {
     return m_model.size();
 }
 
-QVariant ScrollViewModel::data(const QModelIndex& index, int role = Qt::DisplayRole) const {
+QVariant ScrollViewModel::data(const QModelIndex& index, int role) const {
     Q_UNUSED(role)
     if (!hasIndex(index.row(), index.column(), index.parent()))
         return {};
