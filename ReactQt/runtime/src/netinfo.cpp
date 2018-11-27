ef5c0a55a16a9603c424bc4bb02b722fcbb0c48d
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2018-11-27 15:55:48 +0200

    Fix NetInfo module

--- ReactQt/runtime/src/netinfo.cpp
+++ ReactQt/runtime/src/netinfo.cpp
@@ -13,6 +13,9 @@
 
 #include <QDebug>
 #include <QNetworkAccessManager>
+#include <QNetworkConfiguration>
+#include <QNetworkConfigurationManager>
+#include <QNetworkInterface>
 #include <QNetworkReply>
 #include <QNetworkRequest>
 #include <QTimer>
@@ -21,116 +24,106 @@
 
 #include "bridge.h"
 #include "eventdispatcher.h"
+#include "moduleinterface.h"
 #include "netinfo.h"
 
-namespace {
-
-const int NETWORK_REQUEST_TIMEOUT = 3000;
-const int NETWORK_REPLY_CHECK_TIMEOUT = 1000;
-const int MAX_REPLY_CHECK_COUNTER = NETWORK_REQUEST_TIMEOUT / NETWORK_REPLY_CHECK_TIMEOUT;
-} // namespace
+const QString CON_TYPE_NONE = "none";
+const QString CON_TYPE_UNKNOWNN = "unknown";
+const QString CON_TYPE_WIFI = "wifi";
+const QString CON_TYPE_ETHERNET = "ethernet";
+const QString EFCON_TYPE_UNKNOWNN = "unknown";
 
 class NetInfoPrivate : public QObject {
     Q_OBJECT
 public:
-    enum NetworkState { UnkownState = 0, NotAvailable, Available };
-
     NetInfoPrivate();
     ~NetInfoPrivate();
-    void monitorNetworkAccess();
 
-    void setNetworkState(NetworkState state);
+    void startNetworkAccessMonitoring();
+    QVariantMap networkInfo();
+    void setConnectionType(const QString& state);
 
+public slots:
+    void checkInterfaces();
+
+public:
+    QNetworkConfigurationManager networkManager;
     Bridge* bridge = nullptr;
-    QTimer requestTimer;
-    QTimer replyTimer;
-    NetworkState networkState = UnkownState;
+    QString connectionType = CON_TYPE_NONE;
+    QString effectiveConnectionType = EFCON_TYPE_UNKNOWNN;
     int replyStateCheckCounter = 0;
 
 Q_SIGNALS:
     void networkStateChanged();
 };
 
-namespace {
-static QMap<NetInfoPrivate::NetworkState, QString> accessibleName{{NetInfoPrivate::UnkownState, "unknown"},
-                                                                  {NetInfoPrivate::NotAvailable, "none"},
-                                                                  {NetInfoPrivate::Available, "wifi"}};
-} // namespace
-
-void NetInfoPrivate::setNetworkState(NetInfoPrivate::NetworkState state) {
-    if (networkState != state) {
-        networkState = state;
+void NetInfoPrivate::setConnectionType(const QString& type) {
+    if (connectionType != type) {
+        connectionType = type;
         Q_EMIT networkStateChanged();
     }
 }
 
-NetInfoPrivate::NetInfoPrivate() {
-    replyTimer.setInterval(NETWORK_REPLY_CHECK_TIMEOUT);
+NetInfoPrivate::NetInfoPrivate() {}
+
+NetInfoPrivate::~NetInfoPrivate() {}
+
+QVariantMap NetInfoPrivate::networkInfo() {
+    return QVariantMap{{"connectionType", connectionType}, {"effectiveConnectionType", effectiveConnectionType}};
 }
 
-NetInfoPrivate::~NetInfoPrivate() {
-    requestTimer.stop();
-    replyTimer.stop();
-}
+void NetInfoPrivate::checkInterfaces() {
+    auto type = CON_TYPE_NONE;
 
-void NetInfoPrivate::monitorNetworkAccess() {
-    if (requestTimer.isActive())
-        return;
-    QObject::connect(this, &NetInfoPrivate::networkStateChanged, [=]() {
-        bridge->eventDispatcher()->sendDeviceEvent("networkStatusDidChange",
-                                                   QVariantMap{{"connectionType", accessibleName.value(networkState)}});
-    });
+    foreach (QNetworkInterface interface, QNetworkInterface::allInterfaces()) {
+        bool interfaceUp = interface.flags().testFlag(QNetworkInterface::IsUp);
+        bool interfaceNotLoopback = !interface.flags().testFlag(QNetworkInterface::IsLoopBack);
+        bool interfaceNotVM = !interface.humanReadableName().contains("VM");
+        bool interfaceMacValid = (interface.hardwareAddress() != "00:00:00:00:00:00");
 
-    QObject::connect(&requestTimer, &QTimer::timeout, [=]() {
-        QNetworkRequest req(QUrl("http://www.google.com"));
-
-        replyStateCheckCounter = 0;
-        replyTimer.start();
-
-        QNetworkReply* reply = bridge->networkAccessManager()->head(req);
-        auto replyFinishOrTimeout = [=]() {
-            auto replyFinishedNoError = reply->isFinished() ? reply->error() == QNetworkReply::NoError : false;
-            auto previousNetworkState = (networkState == NetInfoPrivate::Available);
-
-            if (++replyStateCheckCounter >= MAX_REPLY_CHECK_COUNTER || replyFinishedNoError) {
-                replyTimer.stop();
-
-                if (replyFinishedNoError != previousNetworkState) {
-                    auto newNetworkState =
-                        replyFinishedNoError ? NetInfoPrivate::Available : NetInfoPrivate::NotAvailable;
-                    qDebug() << "monitorNetworkAccess: setNetworkAccessible "
-                             << "old: " << previousNetworkState << "new: " << newNetworkState;
-                    setNetworkState(newNetworkState);
+        if (interfaceUp && interfaceNotLoopback && interfaceNotVM)
+            foreach (QNetworkAddressEntry entry, interface.addressEntries()) {
+                if (interfaceMacValid && entry.ip().toString().contains(".")) {
+                    if (interface.type() == QNetworkInterface::Wifi)
+                        type = CON_TYPE_WIFI;
+                    else if (interface.type() == QNetworkInterface::Ethernet)
+                        type = CON_TYPE_ETHERNET;
                 }
-                reply->deleteLater();
             }
-        };
+    }
+    setConnectionType(type);
+}
 
-        QObject::connect(&replyTimer, &QTimer::timeout, reply, replyFinishOrTimeout);
-        replyTimer.start();
+void NetInfoPrivate::startNetworkAccessMonitoring() {
+
+    connect(this, &NetInfoPrivate::networkStateChanged, [=]() {
+        if (bridge) {
+            qDebug() << "!! send event";
+            bridge->eventDispatcher()->sendDeviceEvent("networkStatusDidChange", networkInfo());
+        }
     });
 
-    requestTimer.start(NETWORK_REQUEST_TIMEOUT);
+    connect(&networkManager, &QNetworkConfigurationManager::updateCompleted, this, &NetInfoPrivate::checkInterfaces);
 }
+
 void NetInfo::getCurrentConnectivity(const ModuleInterface::ListArgumentBlock& resolve,
                                      const ModuleInterface::ListArgumentBlock& reject) {
     Q_UNUSED(reject);
     Q_D(NetInfo);
+    Q_ASSERT(d->bridge);
+
+    resolve(d->bridge, QVariantList{d->networkInfo()});
 }
 
-NetInfo::NetInfo(QObject* parent) : QObject(parent), d_ptr(new NetInfoPrivate) {}
+NetInfo::NetInfo(QObject* parent) : QObject(parent), d_ptr(new NetInfoPrivate) {
+    d_ptr->startNetworkAccessMonitoring();
+}
 
 NetInfo::~NetInfo() {}
 
 void NetInfo::setBridge(Bridge* bridge) {
     Q_D(NetInfo);
     d->bridge = bridge;
-
-    connect(d->bridge, &Bridge::jsAppStartedChanged, this, [=]() {
-        if (d->bridge->jsAppStarted()) {
-            d->monitorNetworkAccess();
-        }
-    });
 }
 
 QString NetInfo::moduleName() {
