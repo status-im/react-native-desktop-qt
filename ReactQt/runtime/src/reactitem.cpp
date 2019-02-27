bab529b3c548114a267cd986cbaacba1d62de07d
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2019-02-27 19:06:25 +0200

    Fixed background color

--- ReactQt/runtime/src/reactitem.cpp
+++ ReactQt/runtime/src/reactitem.cpp
@@ -436,9 +436,11 @@ void ReactItem::paint(QPainter* painter) {
                     painter->fillPath(ap.subtracted(path), d->backgroundColor);
                 }
             } else {
-                QPainterPath backgroundPath;
-                backgroundPath.addRect(area);
-                painter->fillPath(backgroundPath, d->backgroundColor);
+                if (d->backgroundColor.isValid() && d->backgroundColor.alpha() != 0) {
+                    QPainterPath backgroundPath;
+                    backgroundPath.addRect(area);
+                    painter->fillPath(backgroundPath, d->backgroundColor);
+                }
 
                 d->calculateCornerRects(area, border);
                 QPainterPath path;
