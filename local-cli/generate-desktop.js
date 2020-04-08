e7abfd85496c4a0a8cf7bbcb2de91e078dda2dc9
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-08 12:43:20 +0300

    local-cli updated

--- local-cli/generate-desktop.js
+++ local-cli/generate-desktop.js
@@ -1,35 +1,25 @@
-
-/**
- * Copyright (C) 2016, Canonical Ltd.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- */
-
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var yeoman = require('yeoman-environment');
+const fs = require('fs');
+const path = require('path');
+const copyProjectTemplateAndReplace = require('./generator-desktop').copyProjectTemplateAndReplace;
 
 /**
- * Simple utility for running the android yeoman generator.
+ * Simple utility for running the Windows generator.
  *
  * @param  {String} projectDir root project directory (i.e. contains index.js)
  * @param  {String} name       name of the root JS module for this app
  */
-module.exports = function(projectDir, name) {
-  var oldCwd = process.cwd();
-  process.chdir(projectDir);
+function generateDesktop (projectDir, name) {
 
-  var env = yeoman.createEnv();
-  var generatorPath = path.join(__dirname, 'generator');
-  env.register(generatorPath, 'react:app');
-  var args = ['react:app', name].concat(process.argv.slice(4));
-  env.run(args, {'skip-ios': true, 'skip-android': true}, function() {
-    process.chdir(oldCwd);
-  });
-};
+  if (!fs.existsSync(projectDir)) {
+    fs.mkdirSync(projectDir);
+  }
+  copyProjectTemplateAndReplace(
+    path.join(__dirname, 'generator-desktop', 'templates'),
+    projectDir,
+    name
+  );
+}
+
+module.exports = generateDesktop;
