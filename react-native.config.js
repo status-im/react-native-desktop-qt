e623bcdca28158f889618b3a85244a3bb8ed7d0f
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-04-30 17:48:53 +0300

    Fixed react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  platforms: {
     commands: [
       require('./local-cli/runDesktop/runDesktop'),
     ],
-    desktop-qt: {
-        linkConfig: () => null,
-        projectConfig: (projectRoot, projectParams) => null,
-        dependencyConfig: (projectRoot, dependencyParams) => null,
+    platforms: {
+      "desktop-qt": {
+          linkConfig: () => null,
+          projectConfig: (projectRoot, projectParams) => null,
+          dependencyConfig: (projectRoot, dependencyParams) => null,
       },
-  },
+    },
 };
