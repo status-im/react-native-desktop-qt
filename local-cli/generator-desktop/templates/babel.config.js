c014fa2505f19fb6a88e9ef0c130bb4192c8aafd
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2020-05-06 14:35:50 +0300

    Updated ScrollView and babel.config.js

--- local-cli/generator-desktop/templates/babel.config.js
+++ local-cli/generator-desktop/templates/babel.config.js
@@ -23,9 +23,31 @@ module.exports = {
         ],
         resolvePath(sourcePath, currentFile, opts) {
           let map = new Map([
+            // imports in react-native
             ["../Utilities/Platform",     "../../../react-native-desktop-qt/Libraries/Utilities/Platform"],
             ["./Image",                   "../../../react-native-desktop-qt/Libraries/Image/Image"],
-            ["../../Utilities/Platform",  "../../../../react-native-desktop-qt/Libraries/Utilities/Platform"]
+            ["../../Image/Image",         "../../../../react-native-desktop-qt/Libraries/Image/Image"],
+            ["../../../Image/Image",      "../../../../../react-native-desktop-qt/Libraries/Image/Image"],
+            ["../../Utilities/Platform",  "../../../../react-native-desktop-qt/Libraries/Utilities/Platform"],
+            ["./Platform",  "../../../react-native-desktop-qt/Libraries/Utilities/Platform"],
+            ["../Components/View/PlatformViewPropTypes",  "../../../react-native-desktop-qt/Libraries/Components/View/PlatformViewPropTypes"],
+            ["../Utilities/BackHandler",  "../../../react-native-desktop-qt/Libraries/Utilities/BackHandler"],
+            ["./RCTNetworking",                   "../../../react-native-desktop-qt/Libraries/Network/RCTNetworking"],
+
+
+            // imports in react-native-desktop-qt
+            ["../../Animated/src/AnimatedImplementation", "../../../../react-native/Libraries/Animated/src/AnimatedImplementation"],
+            ["../../Renderer/shims/ReactNative", "../../../../react-native/Libraries/Renderer/shims/ReactNative"],
+            ["../ScrollResponder", "../../../../react-native/Libraries/Components/ScrollResponder"],
+            ["./ScrollViewStickyHeader", "../../../../react-native/Libraries/Components/ScrollView/ScrollViewStickyHeader"],
+            ["./processDecelerationRate", "../../../../react-native/Libraries/Components/ScrollView/processDecelerationRate"],
+            ["../../StyleSheet/StyleSheet", "../../../../react-native/Libraries/StyleSheet/StyleSheet"],
+            ["../View/View", "../../../../react-native/Libraries/Components/View/View"],
+            ["../../Utilities/dismissKeyboard", "../../../../react-native/Libraries/Utilities/dismissKeyboard"],
+            ["../../StyleSheet/flattenStyle", "../../../../react-native/Libraries/StyleSheet/flattenStyle"],
+            ["../../ReactNative/requireNativeComponent", "../../../../react-native/Libraries/ReactNative/requireNativeComponent"],
+            ["../../Image/resolveAssetSource", "../../../../react-native/Libraries/Image/resolveAssetSource"],
+            ["../../StyleSheet/splitLayoutProps", "../../../../react-native/Libraries/StyleSheet/splitLayoutProps"],
           ]);
           if(map.has(sourcePath))
             return map.get(sourcePath);
